94bc8e359289fdac5911625ef646da181e13a212
ms.programmer2938 <78996423+msprogrammer2938@users.noreply.github.com>
2021-02-13 02:49:44 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# This code write by (ms.nope)
+echo "        installing..."
+
+git clone https://github.com/msprogrammer2938/webscan.git
+
+cd webscan
+
+bash install.sh
+# scanweb
