5f7c039e8fa0cd3e56b912932d7abce3129a360b
ms.programmer2938 <78996423+msprogrammer2938@users.noreply.github.com>
2021-03-03 03:01:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/bash
 # This code write by (ms.nope)
 echo "        installing..."
-
-git clone https://github.com/msprogrammer2938/testweb.git
-
-cd webscan
-
-bash install.sh
-
-python3 testweb.git
+echo "        "
+"    ────────╔╗"
+"    ────────║║"
+"    ╔╗╔╗╔╦══╣╚═╦══╦══╦══╦═╗╔═╗╔══╦═╗"
+"    ║╚╝╚╝║║═╣╔╗║══╣╔═╣╔╗║╔╗╣╔╗╣║═╣╔╝"
+"    ╚╗╔╗╔╣║═╣╚╝╠══║╚═╣╔╗║║║║║║║║═╣║"
+"    ─╚╝╚╝╚══╩══╩══╩══╩╝╚╩╝╚╩╝╚╩══╩╝"
+""
+sudo apt install python3
+sudo apt install python
+chmod +x webscanner.py
+echo ""
+echo "finish!"
 # scanweb
